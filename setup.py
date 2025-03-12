2cc0ec2d843a220d34059731731a7acaf62d0877
David Ames Jr. <dames@team.nxlink.com>
2025-03-12 18:41:01 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+try:
+    from sqlbear._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+setup(
+   name="sqlbear",
+   version="0.1.0",
+   packages=find_packages(),
+   install_requires=["pandas", "sqlalchemy", "bson", "pytz"],
+   author="Your Name",
+   author_email="your.email@example.com",
+   description="A pandas extension for easy SQL interaction",
+   long_description=open("README.md").read(),
+   long_description_content_type="text/markdown",
+   url="https://github.com/readytowin2298/sqlbear",
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+   python_requires=">=3.7",
+)
